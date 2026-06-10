827c72be0f77e536aeec740a382a3ee9d3f6ce0c
Arthur Soares <148705578+arthursoares2509@users.noreply.github.com>
2026-06-10 02:47:49 -0300

    Print the sum of two numbers

---
+++ Backend Exercises/Cpp/Print the sum of two numbers/My Solution.cpp
@@ -0,0 +1,8 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main() {
+    cout << "\n\n Print the sum of two numbers :\n";  
+    cout << " The sum of 5 and 5 is : "<< 5+5 <<"\n\n" ; 
+}
---
+++ Backend Exercises/Cpp/Print the sum of two numbers/Sample Solution.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // Including the input-output stream header file
+
+using namespace std; // Using the standard namespace
+
+int main() // Start of the main function
+{
+    cout << "\n\n Print the sum of two numbers :\n"; // Outputting a message for displaying the sum of two numbers
+	cout << "-----------------------------------\n"; // Outputting a separator line
+    cout << " The sum of 29 and 30 is : "<< 29+30 <<"\n\n" ; // Calculating and outputting the sum of 29 and 30
+
+} // End of the main function
