9567a0b184aec3a5c198d3fab6ef62a1181b70eb
Arthur Soares <148705578+arthursoares2509@users.noreply.github.com>
2026-06-16 21:30:24 -0300

    Write a Python program to load the iris data from a given csv file into a dataframe and print the shape of the data, type of the data and first 3 rows

---
+++ Backend Exercises/Machine Learning/Write a Python program to load the iris data from a given csv file into a dataframe and print the shape of the data, type of the data and first 3 rows./Sample Solution.cpp
@@ -0,0 +1,8 @@
+import pandas as pd
+data = pd.read_csv("iris.csv")
+print("Shape of the data:")
+print(data.shape)
+print("\nData Type:")
+print(type(data))
+print("\nFirst 3 rows:")
+print(data.head(3))
